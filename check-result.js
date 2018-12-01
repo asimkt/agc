4f2aec2e8bb93686570fbbb5bca677a124267820
Asim K T <aasimkt@gmail.com>
2018-12-01 19:07:20 +0530

    Basic logic. Checking for kingdom name in message

--- check-result.js
+++ check-result.js
@@ -1,8 +1,24 @@
 function checkMessagesResult(messages) {
-  console.info('Who is the ruler of Southeros?')
-  console.info('King Shan')
+  let containsSecret = false;
+  const formattedMessageObjectsArray = Object.values(messages); //{kingdom: "land", message: "Die or play the tame of thrones"}, {kingdom: "air", message: "Let’s swing the sword together”"}
+  const alliesOfKing = formattedMessageObjectsArray.filter(item => {
+    return isAllLettersIncluded(item.kingdom, item.message);
+  })
+  console.info('\n\nWho is the ruler of Southeros?')
+  console.info(`${alliesOfKing.length > 2 ? 'King Shan' : 'None'}`)
   console.info('Allies of King Shan?')
-  console.info('Air, Land, Ice, Fire')
+  let namesofAlliesOfKing = alliesOfKing.reduce((namesString, item) => {
+    return namesString.concat(item.kingdom + ', ');
+  }, '')
+  // Removes trailing space and comma
+  console.info(namesofAlliesOfKing.trim().slice(0,-1));
+}
+
+function isAllLettersIncluded(kingdom, message) {
+  let sumOfOccurencesOfLetters = kingdom.split("").reduce((acc, item) => {
+    return message.indexOf(item) > -1 ? acc + 1 : acc;
+  }, 0);
+  return sumOfOccurencesOfLetters === kingdom.length;
 }
 
 module.exports = checkMessagesResult;
--- constants.js
+++ constants.js
@@ -2,44 +2,6 @@ const {validateInput, filterValue} = require('./utils.js');
 
 const KINGDOMS = ['land', 'water', 'ice', 'air', 'fire'];
 
-// const QUESTIONS = [
-//   {
-//     type : 'input',
-//     name : 'input1',
-//     validate: validateInput,
-//     filter: filterValue,
-//     message : 'Input: '
-//   },
-//   {
-//     type : 'input',
-//     name : 'input2',
-//     validate: validateInput,
-//     filter: filterValue,
-//     message : 'Input: '
-//   },
-//   {
-//     type : 'input',
-//     name : 'input3',
-//     validate: validateInput,
-//     filter: filterValue,
-//     message : 'Input: '
-//   },
-//   {
-//     type : 'input',
-//     name : 'input4',
-//     validate: validateInput,
-//     filter: filterValue,
-//     message : 'Input: '
-//   },
-//   {
-//     type : 'input',
-//     name : 'input5',
-//     validate: validateInput,
-//     filter: filterValue,
-//     message : 'Input: '
-//   }
-// ];
-
 const QUESTIONS = [
   {
     type : 'input',
@@ -54,8 +16,46 @@ const QUESTIONS = [
     validate: validateInput,
     filter: (val) => filterValue(val, KINGDOMS),
     message : 'Input: '
+  },
+  {
+    type : 'input',
+    name : 'input3',
+    validate: validateInput,
+    filter: (val) => filterValue(val, KINGDOMS),
+    message : 'Input: '
+  },
+  {
+    type : 'input',
+    name : 'input4',
+    validate: validateInput,
+    filter: (val) => filterValue(val, KINGDOMS),
+    message : 'Input: '
+  },
+  {
+    type : 'input',
+    name : 'input5',
+    validate: validateInput,
+    filter: (val) => filterValue(val, KINGDOMS),
+    message : 'Input: '
   }
 ];
+// 
+// const QUESTIONS = [
+//   {
+//     type : 'input',
+//     name : 'input1',
+//     validate: validateInput,
+//     filter: (val) => filterValue(val, KINGDOMS),
+//     message : 'Input: '
+//   },
+//   {
+//     type : 'input',
+//     name : 'input2',
+//     validate: validateInput,
+//     filter: (val) => filterValue(val, KINGDOMS),
+//     message : 'Input: '
+//   }
+// ];
 
 module.exports.KINGDOMS = KINGDOMS;
 module.exports.QUESTIONS = QUESTIONS;
